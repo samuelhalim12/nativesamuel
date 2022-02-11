2a24bc8483aa0a066ee5d83c5e9f039cf0fda3f1
depriandiy <depriandianton22@gmail.com>
2022-02-11 16:37:04 +0700

    profile_dev

--- app/src/main/java/com/ubaya/nativesamuel/Login.kt
+++ app/src/main/java/com/ubaya/nativesamuel/Login.kt
@@ -2,6 +2,6 @@ package com.ubaya.nativesamuel
 
 class Login (var username:String, var password:String){
     override fun toString():String {
-        return "$username $password"
+        return "Username : $username $password"
     }
 }
