7746f8f31a552e8cf41826a01184d2f7ee9b85e1
samuelhalim12 <samuelhalim12@gmail.com>
2022-02-11 15:10:57 +0700

    add Variable abc and logout

--- app/src/main/java/com/ubaya/nativesamuel/MainActivity.kt
+++ app/src/main/java/com/ubaya/nativesamuel/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     // perlu bikin fitur login
     var login: Boolean = false
+    var abc: String = "abc"
+    var logout: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
