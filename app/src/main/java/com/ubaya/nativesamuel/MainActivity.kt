b196d26edfcfc8fadbe638013e0eb46862188c83
Hendri <hendriantonius27@gmail.com>
2022-02-11 16:25:39 +0700

    Tambah spasi

--- app/src/main/java/com/ubaya/nativesamuel/MainActivity.kt
+++ app/src/main/java/com/ubaya/nativesamuel/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     var abc: String = "abc"
     var test ="coba"
     var tamba = ""
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
